d3fdce784dd836d16ae9b99979b159ab10981d5e
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-18 13:56:22 +0600

    let vs const

--- inchToFeet.js
+++ inchToFeet.js
@@ -1,11 +1,11 @@
 function inchToFeet (inch) {
-    var feet = inch / 12; 
+    let feet = inch / 12; 
     return feet; 
 }
 
-var dadaFeet = inchToFeet(156); 
+const dadaFeet = inchToFeet(156); 
 console.log('dada feet :', dadaFeet); 
-var naniFeet = inchToFeet(168); 
+const naniFeet = inchToFeet(168); 
 console.log('nani feet :', naniFeet); 
-var dadiFeet = inchToFeet(288); 
+const dadiFeet = inchToFeet(288); 
 console.log('dadi feet :', dadaFeet); 
---
+++ letVsConst.js
@@ -0,0 +1,14 @@
+let name = 'jav'; 
+if (name.length > 4) {
+    name = 'Juvu'; 
+}
+console.log(name);
+
+
+let age = 15;
+age = 17; 
+age = 19; 
+
+
+const country = 'Bangladesh'; 
+console.log(country);
